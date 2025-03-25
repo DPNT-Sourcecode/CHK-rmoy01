32a4111de234c6018533e8aab4cd35c5c372efa3
sbx_user1051 <sbx_user1051@169.254.62.211>
2025-03-25 22:21:04 +0000

    Tue Mar 25 22:21:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,10 +5,20 @@
 
 
 def checkout(skus):
-    valid_items = {"A","B","C","D"}
-    prices = {"A":50, "B": 30, "C": 20, "D": 15}
+    valid_items = {"A","B","C","D", "E"}
+    prices = {"A":50, "B": 30, "C": 20, "D": 15, "E": 40}
 
-    offers = {"A": (3,130), "B": (2,45)} #stores special offers (offer quantity, offer price)
+    #stores special offers (offer quantity, offer price)
+
+    offers = {
+        "A": [(5,200),(3,130)], 
+        "B": [(2,45)]
+    } 
+
+    #initialises free offer with required quantity and free item alongside each other
+    free_offers = {
+        "E": (2,"B")
+    }
 
     #loop responsible for validating the input
     for x in skus: 
@@ -18,18 +28,24 @@ def checkout(skus):
 
     counter = {item: skus.count(item) for item in valid_items}
 
+    for item, (req_quantity, free_item) in free_offers.items():
+        free_units = counter[item] // req_quantity
+        if free_item in counter:
+            counter[free_item] = max(0, counter[free_item] - free_units)
+
     total = 0
 
-    #loops through each item, gets quantity, checks if item has a special offer, and if so it calculates total price by combining offer prices and remaining item prices
-    
+    #loops through each item, applies any offers and calculates the total
+
     for item in valid_items:
         quantity = counter[item]
         if item in offers:
-            offer_quantity, offer_price = offers[item]
-            total += (quantity // offer_quantity) * offer_price
-            total += (quantity % offer_quantity) * prices[item]
-        else:
-            total += quantity * prices[item]
+            for offer_quantity, offer_price in offers[item]:
+                offer_applied = quantity // offer_quantity
+                total += offer_applied * offer_price
+                quantity %= offer_quantity
+        
+        total += quantity * prices[item]
 
     return total
         
